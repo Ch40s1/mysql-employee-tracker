0423222b6be7779c3003fcb91e7e2bc02f70a097
Daniel Z <114247747+Ch40s1@users.noreply.github.com>
2023-08-23 21:29:06 -0500

    add department now adds a value to the database

--- index.js
+++ index.js
@@ -56,11 +56,17 @@ function startApp() {
 }
 
 function viewAllDepartments() {
-  // creates a connection to mysql. selects all from departments
+  // creates a connection to the mysql server and selects all from the department in employee.db
   dbConnection.query('SELECT * FROM department', (err, results) => {
     if (err) throw err;
-    console.table(results);
-    // restarts the app
+    // this is for the looks
+    console.log('id  department_name');
+    console.log('--  ---------------');
+    // this so that it looks like the '--' are folling the values
+    results.forEach(result => {
+      console.log(`${result.id.toString().padStart(2)}   ${result.department_name}`);
+    });
+    // starts the app again
     startApp();
   });
 }
@@ -74,7 +80,45 @@ function viewAllEmployees() {
 }
 
 function addDepartment() {
+  // asks questions needed for the values
+  inquirer
+  .prompt([
+    {
+      name: 'departmentId',
+      type: 'input',
+      message: 'Enter the department ID:',
+      // makes sure that the id is a converted into an integer and if we typed a valid id number
+      validate: input => {
+        if (Number.isInteger(parseInt(input)) && parseInt(input) > 0) {
+          return true;
+        }
+        return 'Please enter a valid positive integer for the department ID.';
+      }
+    },
+    {
+      name: 'departmentName',
+      type: 'input',
+      message: 'Enter the name of the department:',
+      // makes sure there is no empty string
+      validate: input => {
+        if (input.trim() !== '') {
+          return true;
+        }
+        return 'Please enter a valid department name.';
+      }
+    }
+  ])
+  .then(answers => {
+    const departmentId = parseInt(answers.departmentId);
+    const departmentName = answers.departmentName;
 
+    // acts like the seeds,sql and adds the values to the data
+    dbConnection.query('INSERT INTO department (id, department_name) VALUES (?, ?)', [departmentId, departmentName], (err, result) => {
+      if (err) throw err;
+      console.log(`Department "${departmentName}" with ID ${departmentId} added successfully!`);
+      startApp();
+    });
+  });
 }
 
 function addRole() {
